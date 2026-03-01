de1426f29dc4b3ae9fce95d435f6ea68e27bb3c2
Ethan-Prunty <ethanlprunty@gmail.com>
2026-02-28 18:36:08 -0600

    Update marimocontext.py
    Added .py-file specific commands.

--- marimocontext.py
+++ marimocontext.py
@@ -2,15 +2,19 @@ from context_menu import menus
 from context_menu.menus import ContextMenu
 
 #.py-specific commands
-edit_with_marimo = menus.FastCommand("Edit with marimo", type=".py", command="marimo edit ?", command_vars=["FILENAME"])
-sandboxed_edit_with_marimo = menus.FastCommand("Edit in a sandbox", type=".py", command="marimo edit ? --sandbox", command_vars=["FILENAME"])
-run_with_marimo = menus.FastCommand("Run with marimo", type=".py", command="marimo run ?", command_vars=["FILENAME"])
+python_menu = ContextMenu("Marimo", type=".py")
+edit_with_marimo = menus.ContextCommand("Edit with marimo",  command="marimo edit ?", command_vars=["FILENAME"])
+sandboxed_edit_with_marimo = menus.ContextCommand("Edit in a sandbox", command="marimo edit ? --sandbox", command_vars=["FILENAME"])
+run_with_marimo = menus.ContextCommand("Run with marimo", command="marimo run ?", command_vars=["FILENAME"])
+
+python_menu.add_items([edit_with_marimo, sandboxed_edit_with_marimo, run_with_marimo])
+python_menu.compile()
 
 #General commands
+general_menu = ContextMenu("Marimo", type="DIRECTORY_BACKGROUND")
 create_with_marimo = menus.ContextCommand("Create a new notebook", command="cmd /c marimo new")
 marimo_home = menus.ContextCommand("Open the marimo hub",  command="cmd /c marimo edit")
 
-general_menu = ContextMenu("Marimo", type="DIRECTORY_BACKGROUND")
 general_menu.add_items([create_with_marimo, marimo_home])
 general_menu.compile()
 
