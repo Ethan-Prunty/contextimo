56bb8d047c6235900a8b090acbb322c8eba9b876
Ethan-Prunty <ethanlprunty@gmail.com>
2026-02-28 18:31:08 -0600

    Update marimocontext.py
    Added general commands (`marimo home` and `marimo new`)

--- marimocontext.py
+++ marimocontext.py
@@ -1,5 +1,17 @@
 from context_menu import menus
+from context_menu.menus import ContextMenu
 
-fc = menus.FastCommand("TEST COMMAND", type="FILES", command="cmd /c echo 'Hello world'")
-marimo = menus.FastCommand("Edit with marimo", type=".py", command="cmd /c marimo edit", command_vars=["FILENAME"])
-marimo.compile()
+#.py-specific commands
+edit_with_marimo = menus.FastCommand("Edit with marimo", type=".py", command="marimo edit ?", command_vars=["FILENAME"])
+sandboxed_edit_with_marimo = menus.FastCommand("Edit in a sandbox", type=".py", command="marimo edit ? --sandbox", command_vars=["FILENAME"])
+run_with_marimo = menus.FastCommand("Run with marimo", type=".py", command="marimo run ?", command_vars=["FILENAME"])
+
+#General commands
+create_with_marimo = menus.ContextCommand("Create a new notebook", command="cmd /c marimo new")
+marimo_home = menus.ContextCommand("Open the marimo hub",  command="cmd /c marimo edit")
+
+general_menu = ContextMenu("Marimo", type="DIRECTORY_BACKGROUND")
+general_menu.add_items([create_with_marimo, marimo_home])
+general_menu.compile()
+
+
