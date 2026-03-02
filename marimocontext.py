247a6a798bfc5f8ceba6614ece62a1d5ef367cce
Ethan-P. <ethanlprunty@gmail.com>
2026-03-02 10:38:31 -0600

    Update marimocontext.py
    Refactored; added option to remove all of the created commands.

--- marimocontext.py
+++ marimocontext.py
@@ -1,21 +1,55 @@
+# for getting the absolute path to the marimo icon
+import os.path
+
 from context_menu import menus
 from context_menu.menus import ContextMenu
 
-#.py-specific commands
-python_menu = ContextMenu("Marimo", type=".py", icon_path="C:\\Users\\elp83s\\Desktop\\MarimoContext\\marimo_icon.ico")
-edit_with_marimo = menus.ContextCommand("edit",  command="marimo edit ?", command_vars=["FILENAME"])
-sandboxed_edit_with_marimo = menus.ContextCommand("edit in a sandbox", command="marimo edit ? --sandbox", command_vars=["FILENAME"])
-run_with_marimo = menus.ContextCommand("run", command="marimo run ?", command_vars=["FILENAME"])
 
-python_menu.add_items([edit_with_marimo, sandboxed_edit_with_marimo, run_with_marimo])
-python_menu.compile()
+# adds all of the commands
+def add_commands():
+    # commands which appear in the context menu for .py files
+    python_menu = ContextMenu(
+        "Marimo", type=".py", icon_path=os.path.abspath("marimo_icon.ico")
+    )
 
-#General commands
-general_menu = ContextMenu("Marimo", type="DIRECTORY_BACKGROUND", icon_path="C:\\Users\\elp83s\\Desktop\\MarimoContext\\marimo_icon.ico")
-create_with_marimo = menus.ContextCommand("new notebook", command="cmd /c marimo new")
-marimo_home = menus.ContextCommand("marimo hub",  command="cmd /c marimo edit")
+    edit_with_marimo = menus.ContextCommand(
+        "edit", command="marimo edit ?", command_vars=["FILENAME"]
+    )
+    sandboxed_edit_with_marimo = menus.ContextCommand(
+        "edit in a sandbox",
+        command="marimo edit ? --sandbox",
+        command_vars=["FILENAME"],
+    )
+    run_with_marimo = menus.ContextCommand(
+        "run", command="marimo run ?", command_vars=["FILENAME"]
+    )
 
-general_menu.add_items([create_with_marimo, marimo_home])
-general_menu.compile()
+    python_menu.add_items(
+        [edit_with_marimo, sandboxed_edit_with_marimo, run_with_marimo]
+    )
+    python_menu.compile()
+
+    # commands which appear when in a folder or the desktop
+    general_menu = ContextMenu(
+        "Marimo",
+        type="DIRECTORY_BACKGROUND",
+        icon_path=os.path.abspath("marimo_icon.ico"),
+    )
+
+    create_with_marimo = menus.ContextCommand(
+        "new notebook", command="cmd /c marimo new"
+    )
+    marimo_home = menus.ContextCommand("marimo hub", command="cmd /c marimo edit")
+
+    general_menu.add_items([create_with_marimo, marimo_home])
+    general_menu.compile()
 
 
+# removes all the created menus
+def remove_commands():
+    menus.removeMenu("Marimo", type=".py")
+    menus.removeMenu("Marimo", type="DIRECTORY_BACKGROUND")
+
+
+add_commands()
+# remove_commands()
